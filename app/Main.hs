793a0cd8df27264d030fba050976b7e7f422a459
Keith <hnefatl@gmail.com>
2018-10-06 16:52:37 +0100

    Initial: read a file, lex+parse and print

---
+++ app/Main.hs
@@ -0,0 +1,27 @@
+module Main where
+
+import Control.Monad
+import Language.Haskell.Parser
+import Language.Haskell.Syntax
+import Text.Pretty.Simple
+import System.Environment
+
+main :: IO ()
+main = do
+    files <- getArgs
+    forM_ files compileFile
+
+printParseError :: SrcLoc -> String -> IO ()
+printParseError (SrcLoc file line col) msg =
+    putStrLn $ "File \"" ++ file ++ "\", line " ++ show line ++ ", column " ++ show col ++ ": " ++ msg 
+
+compileFile :: FilePath -> IO ()
+compileFile path = do
+    contents <- readFile path
+    let mode = ParseMode { parseFilename = path }
+    case parseModuleWithMode mode contents of
+        ParseFailed loc msg -> printParseError loc msg
+        ParseOk moduledef -> pPrint moduledef
+
+
+-- Look at thih
