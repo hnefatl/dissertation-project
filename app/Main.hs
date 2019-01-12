4878903a980ba62a43617cf1ee65f13ae1965041
Keith <hnefatl@gmail.com>
2019-01-12 13:18:41 +0000

    Bootstrap methods created and arguments passed: now error in bytecode when executing

--- app/Main.hs
+++ app/Main.hs
@@ -26,33 +26,49 @@ data BootstrapMethod = BootstrapMethod
 instance Binary BootstrapMethod where
     put m = do
         put $ bootstrapMethodRef m
-        put $ (fromIntegral $ length $ bootstrapArguments m :: Word32)
+        put $ (fromIntegral $ length $ bootstrapArguments m :: Word16)
         forM_ (bootstrapArguments m) put -- Don't use the default list instance as it prepends the length
     get = do
         ref <- get
-        numArgs <- get :: Get Word32
+        numArgs <- get :: Get Word16
         args <- replicateM (fromIntegral numArgs) get
         return $ BootstrapMethod { bootstrapMethodRef = ref, bootstrapArguments = args }
 
 main :: IO ()
 main = do
-    ((ani, funIndex), testClass) <- generateIO [] "Test" $ do
+    let classname = "Test"
+    ((ani, funIndex, arg1, arg2s, arg3), testClass) <- generateIO [] classname $ do
+        -- Create the class
         makeTestClass
+        -- Create a reference to the lambda creation method, for use in the bootstrap method
         let sigArgs = map ObjectType
                 ["java/lang/invoke/MethodHandles$Lookup", "java/lang/String", "java/lang/invoke/MethodType", "java/lang/invoke/MethodType", "java/lang/invoke/MethodHandle", "java/lang/invoke/MethodType" ]
-            method = Method
+            metafactoryMethod = Method
                 { methodAccessFlags = S.fromList [ ACC_PUBLIC, ACC_STATIC ]
                 , methodName = "metafactory"
                 , methodSignature = MethodSignature sigArgs (Returns $ ObjectType "java/lang/invoke/CallSite")
                 , methodAttributesCount = 0
-                , methodAttributes = AR $ M.fromList []
+                , methodAttributes = AR M.empty
                 }
         ani <- addToPool (CUTF8 "BootstrapMethods")
-        lamMethHandle <- addToPool (CMethodHandle 6 "java/lang/invoke/LambdaMetaFactory" method)
-        return (ani, lamMethHandle)
+        lamMethHandle <- addToPool (CMethodHandle InvokeStatic "java/lang/invoke/LambdaMetaFactory" metafactoryMethod)
+        arg1 <- addToPool (CMethodType "(Ljava/lang/Object;Ljava/lang/Object;)Ljava/lang/Object;")
+        arg3 <- addToPool (CMethodType "([LHeapObject;[LHeapObject;)LHeapObject;")
+        let methods = [ "foo" ]
+        arg2s <- forM methods $ \name -> do
+            let method = Method
+                    { methodAccessFlags = S.fromList [ ACC_PUBLIC, ACC_STATIC ]
+                    , methodName = name
+                    , methodSignature = MethodSignature [heapObjectClass, heapObjectClass] (Returns heapObjectClass)
+                    , methodAttributesCount = 0
+                    , methodAttributes = AR M.empty }
+            addToPool (CMethodHandle InvokeStatic classname method)
+        return (ani, lamMethHandle, arg1, arg2s, arg3)
     let classFile = classDirect2File testClass
-        newAttributes = [BootstrapMethod { bootstrapMethodRef = funIndex, bootstrapArguments = [] }]
-        payload = encode (genericLength newAttributes :: Word16) <> encode newAttributes
+        newAttributes =
+            [ BootstrapMethod { bootstrapMethodRef = funIndex, bootstrapArguments = [ arg1, arg2, arg3 ] }
+            | arg2 <- arg2s ]
+        payload = encode (genericLength newAttributes :: Word16) <> mconcat (map encode newAttributes)
         bootstrapAttribute = Attribute
             { attributeName = ani
             , attributeLength = fromIntegral $ B.length payload
@@ -62,6 +78,7 @@ main = do
             , classAttributes = AP $ bootstrapAttribute:attributesList (classAttributes classFile) }
     pPrint classFile'
     putStrLn ""
+    print funIndex
     B.writeFile "Test.class" $ encode classFile'
 
 makeTestClass :: GeneratorIO ()
