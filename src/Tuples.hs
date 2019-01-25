13a62b3ba73a26b6c353713c2fb71be081253532
Keith <hnefatl@gmail.com>
2019-01-25 14:16:06 +0000

    Added tuples file

---
+++ src/Tuples.hs
@@ -0,0 +1,7 @@
+module Tuples where
+
+import BasicPrelude hiding (replicate)
+import Data.Text (replicate)
+
+makeTupleName :: Int -> Text
+makeTupleName elems = "(" <> replicate (elems - 1) "," <> ")"
