81c3a01563cbd4824324f48c8b9459f567db54f5
Keith <hnefatl@gmail.com>
2019-02-01 11:34:01 +0000

    Moved java runtime code into a dedicated non-experimental folder, added build hooks to recompile on build

--- Setup.hs
+++ Setup.hs
@@ -1,2 +1,13 @@
-import Distribution.Simple
-main = defaultMain
+import Distribution.PackageDescription (PackageDescription)
+import Distribution.Simple (buildHook, UserHooks, defaultMainWithHooks, simpleUserHooks)
+import Distribution.Simple.LocalBuildInfo (LocalBuildInfo)
+import Distribution.Simple.Setup (BuildFlags)
+import System.Process (callCommand)
+
+main :: IO ()
+main = defaultMainWithHooks $ simpleUserHooks { buildHook = customBuildHook }
+
+customBuildHook :: PackageDescription -> LocalBuildInfo -> UserHooks -> BuildFlags -> IO ()
+customBuildHook desc lbi uh bf = do
+    (buildHook simpleUserHooks) desc lbi uh bf
+    callCommand "javac -classpath runtime runtime/*.java"
--- javaexperiment/_282c29.java
+++
@@ -1,9 +0,0 @@
-// _282c29 is the java-sanitised translation of "(,)"
-public class _282c29 extends BoxedData {
-    public static _282c29 _make282c29(HeapObject x, HeapObject y) {
-        _282c29 p = new _282c29();
-        p.branch = 0;
-        p.data = new HeapObject[] { x, y };
-        return p;
-    }
-}
