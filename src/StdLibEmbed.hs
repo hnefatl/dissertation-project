59b5c54d21f74cab7a5b0151c10364478d2889c8
Keith <hnefatl@gmail.com>
2019-04-11 14:42:22 +0100

    Actually added embedding file

---
+++ src/StdLibEmbed.hs
@@ -0,0 +1,9 @@
+{-# LANGUAGE TemplateHaskell #-}
+
+module StdLibEmbed where
+
+import BasicPrelude (IsString)
+import Data.FileEmbed (embedStringFile)
+
+stdLibContents :: IsString s => s
+stdLibContents = $(embedStringFile "StdLib.hs")
